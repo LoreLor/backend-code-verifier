ffed484504940735d245e896e44ab60fcbf52ae4
Lorena De Armas <lorenadearmas01@gmail.com>
2022-11-07 10:38:33 -0300

    model-katas

---
+++ src/domain/entities/Katas.entity.ts
@@ -0,0 +1,18 @@
+import  mongoose  from 'mongoose';
+
+export const kataEntity = () => {
+
+    let kataSchema = new mongoose.Schema(
+        {
+            name: String,
+            description: String,
+            level: Number,
+            user: String,
+            date: String,
+            valoration: Number,
+            chances: Number
+        }
+    )
+
+    return mongoose.model('Katas', kataSchema)
+}
